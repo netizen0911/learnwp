63d522fc981120d59ba92762122b90609ea8781a
Paul G <paulo.guetan@gmail.com>
2019-02-28 20:43:33 +0800

    Initial commit

---
+++ wp-content/themes/learnwp/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Learn WP</title>
+</head>
+<body <?php body_class(); ?>>
+  <header>
+    <section class="top-bar">
+      <div class="social-media-icons"></div>
+      <div class="search"></div>
+    </section>
+    <section class="menu-area">
+      <section class="logo"></section>
+      <nav class="main-nav"></nav>
+    </section>
+  </header>
+  <div class="content-area">
+    <main>
+      <section class="slide"></section>
+      <section class="services"></section>
+      <section class="middle-area">
+        <aside class="sidebar"></aside>
+        <div class="news"></div>
+      </section>
+      <section class="map"></section>
+    </main>
+  </div>
+  <footer></footer>
+</body>
+</html>
