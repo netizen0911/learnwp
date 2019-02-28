4fde6fbd8ef8f911552124aa4658cd48438fb2a9
Paul G <paulo.guetan@gmail.com>
2019-02-28 21:10:50 +0800

    Set up initial theme files

--- wp-content/themes/learnwp/index.php
+++ wp-content/themes/learnwp/index.php
@@ -1,33 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Learn WP</title>
-</head>
-<body <?php body_class(); ?>>
-  <header>
-    <section class="top-bar">
-      <div class="social-media-icons"></div>
-      <div class="search"></div>
+<?php get_header(); ?>
+<div class="content-area">
+  <main>
+    <section class="slide">Slide</section>
+    <section class="services">Services</section>
+    <section class="middle-area">
+      <aside class="sidebar">Sidebar</aside>
+      <div class="news">News</div>
     </section>
-    <section class="menu-area">
-      <section class="logo"></section>
-      <nav class="main-nav"></nav>
-    </section>
-  </header>
-  <div class="content-area">
-    <main>
-      <section class="slide"></section>
-      <section class="services"></section>
-      <section class="middle-area">
-        <aside class="sidebar"></aside>
-        <div class="news"></div>
-      </section>
-      <section class="map"></section>
-    </main>
-  </div>
-  <footer></footer>
-</body>
-</html>
+    <section class="map">Map</section>
+  </main>
+</div>
+<?php get_footer(); ?>
